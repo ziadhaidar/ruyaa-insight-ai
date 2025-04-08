877a957a68a647d62d0aca6fc87b87d8526f92af
ziadhaidar <zizoohaidar84@gmail.Com>
2025-04-08 17:52:49 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,5 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import App from './App.tsx';
-import Register from './pages/Register.tsx';
-import Login from './pages/Login.tsx';
-import Settings from './pages/Settings.tsx';
-import Home from './pages/Home.tsx';
-import Dreams from './pages/Dreams.tsx';
-
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/dreams" element={<Dreams />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);

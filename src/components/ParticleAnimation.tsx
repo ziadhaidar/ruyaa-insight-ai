e23a904028d3d0c85e3efa73f1871036a15c74b2
ziadhaidar <zizoohaidar84@gmail.Com>
2025-04-23 16:29:42 +0200

    Update ParticleAnimation.tsx

--- src/components/ParticleAnimation.tsx
+++ src/components/ParticleAnimation.tsx
@@ -15,7 +15,7 @@ const ParticleAnimation: React.FC<ParticleAnimationProps> = ({
   size = 'h-60 w-60',
   className = '',
   modelUrl = '/girlhead/scene.gltf',
-  particleCount = 800000,
+  particleCount = 100000,
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const pointsRef = useRef<THREE.Points>();
@@ -26,7 +26,7 @@ const ParticleAnimation: React.FC<ParticleAnimationProps> = ({
 
     // 1) Scene, camera, renderer
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(50, container.clientWidth / container.clientHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(30, container.clientWidth / container.clientHeight, 0.1, 1000);
     camera.position.set(0, 0, 200);
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });

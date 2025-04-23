0990af76add96e7758ab4b8f5f69c5e97205ab36
ziadhaidar <zizoohaidar84@gmail.Com>
2025-04-23 16:19:46 +0200

    Update ParticleAnimation.tsx

--- src/components/ParticleAnimation.tsx
+++ src/components/ParticleAnimation.tsx
@@ -26,7 +26,7 @@ const ParticleAnimation: React.FC<ParticleAnimationProps> = ({
 
     // 1) Scene, camera, renderer
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(80, container.clientWidth / container.clientHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(180, container.clientWidth / container.clientHeight, 0.1, 1000);
     camera.position.set(0, 0, 4);
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });

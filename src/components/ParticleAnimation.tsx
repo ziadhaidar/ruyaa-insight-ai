0dd19484529f0235f455d348eb2385d9d4241b81
ziadhaidar <zizoohaidar84@gmail.Com>
2025-04-24 13:03:22 +0200

    Update ParticleAnimation.tsx

--- src/components/ParticleAnimation.tsx
+++ src/components/ParticleAnimation.tsx
@@ -39,7 +39,7 @@ interface ParticleAnimationProps {
 const ParticleAnimation: React.FC<ParticleAnimationProps> = ({
   size = 'h-80 w-80',            // container size
   className = '',
-  modelUrl = '/girlhead/scene.gltf',
+  modelUrl = '/girlhead/mask.gltf',
   particleCount = 20000,
 
   // animations

4b01951c8b8094c6e660bc54116742dddfea62ee
ziadhaidar <zizoohaidar84@gmail.Com>
2025-04-24 13:31:46 +0200

    Update ParticleAnimation.tsx

--- src/components/ParticleAnimation.tsx
+++ src/components/ParticleAnimation.tsx
@@ -46,11 +46,11 @@ const ParticleAnimation: React.FC<ParticleAnimationProps> = ({
 
   // Default animation props
   swingSpeed = 3,              // speed of the swing (min:0, max:5)
-  swingAngle = Math.PI / 100,    // max angle in radians (~1.8°, min:0, max:π/2)
+  swingAngle = Math.PI / 1,    // max angle in radians (~1.8°, min:0, max:π/2)
   breathSpeed = 0.8,             // speed of breathing rotation (min:0, max:5)
   pulseStrength = 1,             // frequency of scale pulse (min:0, max:10)
-  zoomSpeed = 0.1,               // speed of zoom in/out (min:0, max:5)
-  zoomAmp = 10,                  // amplitude of zoom (min:0, max:200)
+  zoomSpeed = 0.2,               // speed of zoom in/out (min:0, max:5)
+  zoomAmp = 20,                  // amplitude of zoom (min:0, max:200)
   particleSize = 0.03,           // size of points (min:0.01, max:1)
 
   // Default shading props
